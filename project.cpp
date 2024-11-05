f0636447b24291c49867b7fd9f68b4a9ee1fb5cf
vladsimonchuk <vladislav.simonchuk@gmail.com>
2024-11-05 17:37:08 +0100

    Dodano funkcje czy_pierwsza

--- project.cpp
+++ project.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+
+bool czy_pierwsza(int liczba) {
+    if (liczba < 2) return false;
+    for (int i = 2; i * i <= liczba; ++i) {
+        if (liczba % i == 0) return false;
+    }
+    return true;
+}
+
 int main() {
     int liczba;
     std::cout << "Podaj liczbe: ";
     std::cin >> liczba;
-    std::cout << "Wprowadzona liczba: " << liczba << std::endl;
+    std::cout << "Wprowadzona liczba: " << liczba;
+    if (czy_pierwsza(liczba)) {
+        std::cout << " jest liczba pierwsza." << std::endl;
+    } else {
+        std::cout << " nie jest liczba pierwsza." << std::endl;
+    }
     return 0;
 }
