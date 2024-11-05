123913800aca943df17c3059927094180cc7f7d5
vladsimonchuk <vladislav.simonchuk@gmail.com>
2024-11-05 17:33:12 +0100

    Dodano project.cpp

---
+++ project.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main() {
+    int liczba;
+    std::cout << "Podaj liczbe: ";
+    std::cin >> liczba;
+    std::cout << "Wprowadzona liczba: " << liczba << std::endl;
+    return 0;
+}
